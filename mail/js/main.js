db1743904c43e75f2166eb79dff110260318d0ad
coluccifrancesco <coluccif2002@gmail.com>
2025-06-03 09:47:32 +0200

    Fix email validation logic and improve console messages

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,14 +1,14 @@
 //  Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, 
-let emailList = ["coluccif2002@gmail.com" , "pincopallo@outlook.com", "jsprobablysucks@reasonablecrashout.com"];
+let emailList = ["coluccif2002@gmail.com" , "pincopallo@outlook.com", "jsSucks@reasonablecrashout.com"];
 
 let emailPrompt = prompt("Inserisci la tua email gentilmente");
 
-for (let i = 0; i <= emailList.length; i++){
+for (let i = 0; i < emailList.length; i++){
     if(emailPrompt == emailList[i]){
-        console.log('Email controllata ed approvata!');
+        console.log('Email accettata: Benvenuto!')
         break
     } else {
-        continue
+        console.log('Email rifiutata: riprova')
     }
 }
 //  stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for
